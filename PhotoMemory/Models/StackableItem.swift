f46ca3533d83d0c10ef066586c8f380fcbe7e7f0
YangHan-Morningstar <imuyanghan@gmail.com>
2020-07-27 00:41:16 +0800

    添加图片资源以及数据的Model与ViewModel

---
+++ PhotoMemory/Models/StackableItem.swift
@@ -0,0 +1,14 @@
+//
+//  StackableItem.swift
+//  PhotoMemory
+//
+//  Created by Tony Young on 2020/7/27.
+//  Copyright © 2020 YangHan-Morningstar. All rights reserved.
+//
+
+import Foundation
+
+struct StackableItem: Identifiable {
+    var id = UUID()
+    var imageName: String
+}
---
+++ PhotoMemory/ViewModels/DataViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  DataViewModel.swift
+//  PhotoMemory
+//
+//  Created by Tony Young on 2020/7/27.
+//  Copyright © 2020 YangHan-Morningstar. All rights reserved.
+//
+
+import Foundation
+
+class DataViewModel: ObservableObject {
+    @Published var itemStacks: [[StackableItem]]
+    
+    init() {
+        itemStacks = [
+            [
+                StackableItem(imageName: "hai1"),
+                StackableItem(imageName: "hai2"),
+                StackableItem(imageName: "hai3"),
+            ],
+            [
+                StackableItem(imageName: "feng1"),
+                StackableItem(imageName: "feng2"),
+                StackableItem(imageName: "feng3"),
+            ],
+            [
+                StackableItem(imageName: "gui1"),
+                StackableItem(imageName: "gui2"),
+                StackableItem(imageName: "gui3"),
+                StackableItem(imageName: "gui4"),
+            ]
+        ]
+    }
+    
+    
+}
