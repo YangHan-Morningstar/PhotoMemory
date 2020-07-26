2707ac848548d217fdade05338a544e116b8438d
YangHan-Morningstar <imuyanghan@gmail.com>
2020-07-27 00:23:05 +0800

    初始化

---
+++ PhotoMemory/Views/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  PhotoMemory
+//
+//  Created by Tony Young on 2020/7/27.
+//  Copyright © 2020 YangHan-Morningstar. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
